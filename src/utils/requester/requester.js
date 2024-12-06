a71b478fa78054dae6ceb4c4d1dcd272b916978d
kanykei0 <duishenalievak@gmail.com>
2024-12-06 19:05:55 +0600

    created requester

---
+++ src/utils/requester/requester.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { BASE_URL } from "../constants/constans";
+import { create } from "zustand";
+
+export const requester = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const useApiStore = create((set) => ({
+  loading: false,
+  error: null,
+  data: [],
+  success: null,
+
+  setLoading: (loading) => set({ loading }),
+  setError: (error) => set({ error }),
+  setData: (data) => set({ data }),
+  setSuccess: (success) => set({ success }),
+
+  resetSuccess: () => set({ success: null }),
+  resetError: () => set({ error: null }),
+
+  fetchData: async (url) => {
+    set({ loading: true });
+    try {
+      const { data } = await requester.get(url);
+
+      if (!data) {
+        throw new Error("Not Found");
+      }
+      set({ data });
+      return data;
+    } catch (error) {
+      set({ error: error.message });
+      throw error;
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  postRequest: async (url, data) => {
+    set({ loading: true, success: null, error: null });
+    try {
+      await requester.post(url, data);
+      set({ success: "Request successful" });
+    } catch (error) {
+      set({
+        error: error.message,
+        success: null,
+      });
+
+      throw error;
+    } finally {
+      set({ loading: false });
+    }
+  },
+}));
