0e3b22fdce1cc90ccda1e388b8f1b4d1d1d8575c
Temirlan Dzhumanov <temadjumanov@gmail.com>
2024-12-06 21:32:33 +0600

    improved form

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -2,40 +2,59 @@ import { useState } from "react";
 import { Typography, Container, Button } from "ui/index";
 import classes from "./Form.module.scss";
 import formImage from "../../assets/images/formImage.png";
-import Select from "react-select";
 import {
   optionsCountry,
   optionsSpeciality,
   optionsStudy,
 } from "utils/constants/Constants";
+import { TextField, MenuItem } from "@mui/material";
+import { IMaskInput } from "react-imask";
 
 export const Form = () => {
   const [state, setState] = useState({
     name: "",
     number: "",
-    country: null,
-    study: null,
-    speciality: null,
+    country: "",
+    study: "",
+    speciality: "",
   });
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
+
     setState((prevState) => ({
       ...prevState,
       [name]: value,
     }));
   };
 
-  const handleSelectChange = (name) => (selectedOption) => {
-    setState((prevState) => ({
-      ...prevState,
-      [name]: selectedOption,
-    }));
-  };
-
   const onFormSubmit = (e) => {
     e.preventDefault();
-    console.log(state);
+    const formatState = {
+      ...state,
+      number: "996" + state.number,
+    };
+    console.log("Form Submitted", formatState);
+  };
+
+  const sharedTextFieldStyles = {
+    textAlign: "start",
+    "& .MuiOutlinedInput-root": {
+      height: "45px",
+      borderRadius: "8px",
+      background: "var(--color-white)",
+      outline: "none",
+    },
+    "& .MuiOutlinedInput-input": {
+      padding: "10px",
+    },
+    "& .MuiInputLabel-root": {
+      lineHeight: "1.2",
+      top: "-4px",
+    },
+    "& .MuiInputLabel-shrink": {
+      top: "0px",
+    },
   };
 
   return (
@@ -51,77 +70,88 @@ export const Form = () => {
             </Typography>
 
             <form className={classes.form} onSubmit={onFormSubmit}>
-              <input
-                type="text"
+              <TextField
                 name="name"
-                className={classes.formInput}
-                placeholder="ФИО"
+                label="ФИО"
+                variant="outlined"
                 value={state.name}
                 onChange={handleInputChange}
+                sx={sharedTextFieldStyles}
               />
 
-              <input
-                type="text"
+              <TextField
                 name="number"
-                className={classes.formInput}
-                placeholder="Номер телефона"
+                label="Номер телефона"
+                variant="outlined"
                 value={state.number}
-                onChange={handleInputChange}
+                slotProps={{
+                  input: {
+                    inputComponent: IMaskInput,
+                    inputProps: {
+                      mask: "+996 (000) 000 000",
+                      onAccept: (value) => {
+                        setState((prevState) => ({
+                          ...prevState,
+                          number: value,
+                        }));
+                      },
+                      unmask: true,
+                    },
+                  },
+                }}
+                sx={sharedTextFieldStyles}
               />
 
-              <Select
+              <TextField
+                id="country"
                 name="country"
-                options={optionsCountry}
-                isMulti
-                placeholder="Страна"
+                select
+                label="Страна"
                 value={state.country}
-                onChange={handleSelectChange("country")}
-                styles={{
-                  control: (base) => ({
-                    ...base,
-                    minHeight: "45px",
-                    border: "1.5px solid var(--color-primary)",
-                    borderRadius: "8px",
-                    textAlign: "start",
-                  }),
-                }}
-              />
+                defaultValue={""}
+                sx={sharedTextFieldStyles}
+                onChange={handleInputChange}
+              >
+                {optionsCountry.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
 
-              <Select
+              <TextField
+                id="study"
                 name="study"
-                options={optionsSpeciality}
-                isMulti
-                placeholder="Направление обучения"
+                select
+                label="Направление обучения"
                 value={state.study}
-                onChange={handleSelectChange("study")}
-                styles={{
-                  control: (base) => ({
-                    ...base,
-                    minHeight: "45px",
-                    border: "1.5px solid var(--color-primary)",
-                    borderRadius: "8px",
-                    textAlign: "start",
-                  }),
-                }}
-              />
+                defaultValue={""}
+                sx={sharedTextFieldStyles}
+                onChange={handleInputChange}
+              >
+                {optionsStudy.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
 
-              <Select
+              <TextField
+                id="speciality"
                 name="speciality"
-                options={optionsStudy}
-                isMulti
-                placeholder="Желаемая специальность"
+                select
+                label="Желаемая специальность"
                 value={state.speciality}
-                onChange={handleSelectChange("speciality")}
-                styles={{
-                  control: (base) => ({
-                    ...base,
-                    minHeight: "45px",
-                    border: "1.5px solid var(--color-primary)",
-                    borderRadius: "8px",
-                    textAlign: "start",
-                  }),
-                }}
-              />
+                defaultValue={""}
+                sx={sharedTextFieldStyles}
+                onChange={handleInputChange}
+              >
+                {optionsSpeciality.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
 
               <Button type="submit" size="default" fullWidth={true}>
                 <Typography variant="h4">Отправить заявку</Typography>
